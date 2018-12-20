db9714f2f738f08310d0f3ae47a0437172af4572
罗琦 <401997126@qq.com>
2018-12-20 20:14:40 +0800

    Initial commit

---
+++ app/router.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+// module.exports = app => {
+//   const { router, controller } = app;
+//   router.get('/', controller.home.index);
+// };
+
+// app/router.js
+module.exports = app => {
+  const { router, controller } = app;
+  router.resources('users', '/users', controller.user);
+};
---
+++ config/config.default.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {
+    security: {
+      csrf: {
+        enable: false,
+      },
+    },
+  };
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1545275384587_3820';
+
+  // add your config here
+  config.middleware = [];
+
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'egg-sequelize-doc-default',
+    username: 'root',
+    password: '12345678',
+  };
+  return config;
+};
